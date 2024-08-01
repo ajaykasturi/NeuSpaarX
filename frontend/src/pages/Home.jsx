6c016bc66a4c1cf542b1a0d254aa00b352debe32
Ajay <ajayreigns0@gmail.com>
2024-08-02 01:51:57 +0530

    add home background and images

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import bgImgHome from "../assets/images/bgimg.png";
+import aiHands from "../assets/images/aihands2.png";
 function Home() {
   return (
     <>
       <Navbar />
-      <div className="h-screen absolute -z-10 w-full border-4 border-slate-950">
+      <div className="h-screen absolute -z-10 w-full">
         <img src={bgImgHome} className="w-full h-full object-cover" />
+      </div>
+      <div className="flex items-center justify-end gap-x-8">
+        <div className="text-9xl pl-10 font-light">
+          Unleash the <br /> Power of <span className="font-black">AI</span>{" "}
+        </div>
+        <div className="min-h-fit">
+          <img src={aiHands} className="h-full w-full object-cover" />
+        </div>
       </div>
     </>
   );
