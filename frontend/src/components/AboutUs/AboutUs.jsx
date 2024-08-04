4bc6b5de1461ad1e062b6f1e52c1b51caedffca4
Ajay <ajayreigns0@gmail.com>
2024-08-05 02:27:24 +0530

    Develop About us Page

---
+++ frontend/src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import aboutBg from "../../assets/images/aboutus.png";
+function AboutUs() {
+  return (
+    <div className="flex gap-x-24 justify-between mx-20 max-[1050px]:flex-col max-[1050px]:gap-y-10 max-[1050px]:items-center">
+      <div className="rounded-2xl min-[650px]:w-[28rem] min-[1051px]:h-[35rem]  min-[1051px]:min-w-fit">
+        <img
+          src={aboutBg}
+          className="w-full h-full rounded-2xl"
+          alt="about us image"
+        />
+      </div>
+      <div className="flex flex-col items-start justify-center gap-y-6 max-[1050px]:items-center">
+        <div className="font-Poppins text-6xl font-normal max-[500px]:text-4xl">
+          About Us
+        </div>
+        <div className="text-justify text-wrap">
+          We are a team of trusted tech enthusiastic people inspired to do more,
+          learn more, and help business ideas translate into reality, and hold a
+          rich experience in collaborating, designing, implementing, and
+          building quality solutions for a wide range of businesses.
+        </div>
+        <div className="rounded-full py-3 px-5 cursor-pointer hover:opacity-90  bg-companycolor text-white text-[1rem]">
+          Know More
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -3,10 +3,12 @@ import Navbar from "../components/Navbar/Navbar";
 import Home from "../pages/Home";
 import Footer from "../components/Footer/Footer";
 import WeFocusOn from "../components/WeFocusOn/WeFocusOn";
+import AboutUs from "../components/AboutUs/AboutUs";
 function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <AboutUs />
       <WeFocusOn />
       <Footer />
     </div>
