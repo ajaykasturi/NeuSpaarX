695b0f240377bb0ba7765deac4b864b0561c9d61
Ajay <ajayreigns0@gmail.com>
2024-08-07 23:44:25 +0530

    Config 6D process images and content

--- frontend/src/components/6DProcess/D6Card.jsx
+++ frontend/src/components/6DProcess/D6Card.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import D6Img from "../../assets/images/d6process.png";
-function D6Card() {
+
+function D6Card({ card }) {
   return (
-    <div className="grid grid-cols-2 grid-rows-1 gradient-border rounded-2xl break-words [aspect-ratio: 75 / 32]">
+    <div className="grid grid-cols-2 grid-rows-1 gradient-border rounded-2xl break-words [aspect-ratio: 75 / 32] h-[32rem]">
       <div className="bg-D6Img rounded-l-2xl">
-        <img src={D6Img} className="w-full h-full object-contain" />
+        <img src={card.img} className="w-full h-full object-contain" />
       </div>
       <div className="flex flex-col gap-y-6 items-start justify-center p-12">
         <div className="text-[#1B2C59] font-Poppins text-7xl font-normal">
-          Discover
+          {card.title}
         </div>
         <div className="text-[#1B2C59] font-Poppins  text-2xl font-normal">
-          We meet clients and discover what they are doing currently and what
-          they want us to do.
+          {card.content}
         </div>
       </div>
     </div>
--- frontend/src/components/6DProcess/D6Process.jsx
+++ frontend/src/components/6DProcess/D6Process.jsx
@@ -1,13 +1,57 @@
 import React from "react";
 import D6Card from "./D6Card";
-
+import defineImg from "../../assets/6DProcessImages/define.png";
+import developImg from "../../assets/6DProcessImages/develop.png";
+import designImg from "../../assets/6DProcessImages/design.png";
+import deployImg from "../../assets/6DProcessImages/deploy.png";
+import deliverImg from "../../assets/6DProcessImages/deliver.png";
 function D6Process() {
+  const config = [
+    {
+      id: 1,
+      title: "Define",
+      content:
+        "We will define the problem and share it with the required audience so that everyone are on the same page.",
+      img: defineImg,
+    },
+    {
+      id: 2,
+
+      title: "Design",
+      content:
+        "Once the problem is defined, we will design a quality solution and for any changes/enhancements required, we will iterrate as needed.",
+      img: designImg,
+    },
+    {
+      id: 3,
+      title: "Develop",
+      content:
+        "Once the design is finalized, we will start developing the solution step by step.",
+      img: developImg,
+    },
+    {
+      id: 4,
+      title: "Deploy",
+      content:
+        "We will deploy a live version of  solution, so that clients can view how the solution works and provide feedback.",
+      img: deployImg,
+    },
+    {
+      id: 6,
+      title: "Deliver",
+      content:
+        "Once the clients are satisfied with the solution deployed, we will deliver the project.",
+      img: deliverImg,
+    },
+  ];
   return (
     <div className="flex flex-col gap-y-16  mx-[8vw] my-16">
       <div className="text-[#1B2C59] text-6xl font-Poppins text-center">
         Our 6-D Process
       </div>
-      <D6Card />
+      {config.map((card) => (
+        <D6Card key={card.id} card={card} />
+      ))}
     </div>
   );
 }
